2485ca1eb75c6ca97399c76c23394c1959f49e9b
Nicha <5316684322rd@gmail.com>
2021-09-30 12:39:02 +0700

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,28 +9,6 @@ $( window ).on('scroll', function(){
 
 
 
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        stagePadding: 20,
-        loop:false,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false,
-                
-            }
-        }
-    });
-  });
   
 
 
